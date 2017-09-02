e4f0ad4f34c1f8f9ba16cb5a65afa9bf68ad0339
FLYCW-WXY <wxyedward@gmail.com>
2017-09-02 15:43:36 +0800

    Add some commentaries.

--- getXJTUAttendanceInfo.py
+++ getXJTUAttendanceInfo.py
@@ -2,6 +2,8 @@
 import requests
 from bs4 import BeautifulSoup
 url='http://wechat.xanet.edu.cn/attendance.jsp'
+
+#按学号获取考勤刷卡信息
 def getXJTUAttendanceData(number):
     try:
         resultList=[]
@@ -13,4 +15,4 @@ def getXJTUAttendanceData(number):
     wholeTable=s.find_all('td',{'style':'text-align:center;word-break:break-all;'})
     for i in range(0,len(wholeTable),2):
         resultList.append({'time':wholeTable[i].string,'location':wholeTable[i+1].string})
-    return resultList
+    return resultList#返回格式[{时间(time),地点(location)}]
--- getXJTUNews.py
+++ getXJTUNews.py
@@ -5,6 +5,7 @@ import bs4
 edunewsbaseurl='http://due.xjtu.edu.cn/jxxx/xytz/'#教学信息 学业通知的基础地址
 libnewsurl='http://mc.m.5read.com/weixin/customize_showList.jspx?schoolId=63&protocol=31'#图书馆新闻地址
 activitiesurl='http://oa.xjtu.edu.cn/form/user/anonymous/dxhd_index.jsp'#校园大型活动的地址
+
 def getHTMLContent(url):
     try:
         r=requests.get(url,timeout=30)
@@ -13,25 +14,31 @@ def getHTMLContent(url):
         return r.text
     except:
         return ""
-def getXJTUEduNews(type):
+
+#获取教务新闻
+def getXJTUEduNews(type):#'xjgl':'学籍管理','ksap':'考试安排','kcap':'课程安排','xjjl':'校际交流','jsap':'竞赛安排','zyfx':'专业辅修','dcxm':'大创项目'
     resultList=[]
     if type=='':
         s = BeautifulSoup(getHTMLContent('http://due.xjtu.edu.cn/jxxx/xytz.htm'),
-                          'html.parser')  # 学籍管理、考试安排、课程安排、校际交流、竞赛安排、专业辅修、大创项目
+                          'html.parser')
         for div in s.find_all('div', {'class': 'list-li'}):
             resultList.append({'date': div('span')[0].string, 'title': div('a')[0].attrs['title'],
                                'link': 'http://due.xjtu.edu.cn' + div('a')[0].attrs['href'][2:]})
-        return resultList  # 返回格式[{日期(date),新闻标题(title),链接(link)}]
-    s=BeautifulSoup(getHTMLContent(edunewsbaseurl+type+'.htm'),'html.parser')#学籍管理、考试安排、课程安排、校际交流、竞赛安排、专业辅修、大创项目
+        return resultList  #返回格式[{日期(date),新闻标题(title),链接(link)}]
+    s=BeautifulSoup(getHTMLContent(edunewsbaseurl+type+'.htm'),'html.parser')
     for div in s.find_all('div',{'class':'list-li'}):
         resultList.append({'date':div('span')[0].string,'title':div('a')[0].attrs['title'],'link':'http://due.xjtu.edu.cn'+div('a')[0].attrs['href'][5:]})
     return resultList#返回格式[{日期(date),新闻标题(title),链接(link)}]
+
+#获取图书馆公告
 def getXJTULibNews():
     resultList=[]
     s=BeautifulSoup(getHTMLContent(libnewsurl),'html.parser')
     for li in s.find_all('li'):
         resultList.append({'date':li('em')[0].string,'title':li('p',{'class':'title'})[0].string,'link':li('a')[0].attrs['href']})
     return resultList#返回格式[{日期(date),新闻标题(title),链接(link)}]
+
+#获取校园活动
 def getXJTUCampAct():
     resultList=[]
     s=BeautifulSoup(getHTMLContent(activitiesurl),'html.parser')#学籍管理、考试安排、课程安排、校际交流、竞赛安排、专业辅修、大创项目
